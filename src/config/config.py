099a8228052e38be32cc8e9c28696ad40e7390da
Thuy Van <tangvan997@gmail.com>
2021-03-16 14:54:13 +0700

    van update code

--- src/config/config.py
+++ src/config/config.py
@@ -1,8 +1,21 @@
+import datetime
+import os
+
+x = datetime.datetime.now()
+a = x.strftime("%B") +" "+ str(x.day) +" "+ str(x.year)
+
+def check_folder(name_folder):
+    list_folder = os.listdir()
+    return name_folder in list_folder
+
+os.chdir('C:/Users/Administrator/Desktop/task1/Migration-Data/src/')
+
+if check_folder(a) == False:
+    os.mkdir(a)
+
 HOST = "localhost"
 USER = "postgres"
 PASSWORD = "vantang123"
 DATABASE = "Car"
-car_table = 'C:/Users/Administrator/Desktop/task1/car_sample_data1.csv'
-bad_table = 'C:/Users/Administrator/Desktop/task1/car_sample_data_bad.csv'
-native_table = 'C:/Users/Administrator/Desktop/task1/car_sample_data_native.csv'
-native1_table = 'C:/Users/Administrator/Desktop/task1/car_sample_data_native1.csv'
+bad_table = 'C:/Users/Administrator/Desktop/task1/Migration-Data/src/'+ a +'/car_sample_data_bad.csv'
+native_table = 'C:/Users/Administrator/Desktop/task1/Migration-Data/src/'+ a +'/car_sample_data_native.csv'
--- src/controller/controller.py
+++ src/controller/controller.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from config import config as cf
 from service.model import ConnPostgre
@@ -10,32 +10,25 @@ from datetime import date
 app = Flask(__name__)
 app.debug = True
 
-tb = cf.car_table
 bad_tb = cf.bad_table
 na_tb = cf.native_table
-na1_tb = cf.native1_table
 
-
-# dt = pd.read_csv(na_tb)
-# natb = pd.DataFrame(dt)
-
-data = pd.read_csv(tb)
-dataFrame = pd.DataFrame(data)
-conn = ConnPostgre(cf.HOST, cf.USER, cf.PASSWORD, cf.DATABASE)
-mess = "success"
-query = Validation()
-
-
-# if request.files['file']:
-#   print(request.files.get['file'])
-
-@app.route('/insert')
-def insert():
+@app.route('/validate', methods=['POST'])
+def update():
+    mess = ''
     try:
-        data = pd.read_csv(na_tb)
-        dataFrame = pd.DataFrame(data)
-        conn = ConnPostgre(cf.HOST, cf.USER, cf.PASSWORD, cf.DATABASE)
-        conn.sql_insert('car', dataFrame)
+        if request.files['file']:
+            file = request.files.get('file')
+            df_native, df_bad = check(file)
+
+            df_bad.to_csv(bad_tb, index=False)
+            df_native.to_csv(na_tb, index=False)
+
+            data = pd.read_csv(na_tb)
+            dataFrame = pd.DataFrame(data)
+            conn = ConnPostgre(cf.HOST, cf.USER, cf.PASSWORD, cf.DATABASE)
+            conn.sql_insert('car', dataFrame)
+        
         return 'success'
     except Exception as ex:
         mess = ex
@@ -43,13 +36,16 @@ def insert():
              'status' : 'connect failed'  }
 
 
-@app.route('/query')
-def check():
+@app.route('/query', methods=['POST'])
+def check(file_csv):
+    data = pd.read_csv(file_csv)
+    dataFrame = pd.DataFrame(data)
+
+    query = Validation()
     mess = "success"
     try:
         df_native = query.day_update(dataFrame)
         check_int_bad, df_native = query.check_int(df_native)
-        # price_check_price_bad, df_native = query.check_price(df_native)
         check_length_bad, df_native = query.check_length(df_native)
         check_dup_bad, df_native = query.check_dup(df_native)
         re_na = query.re_name(df_native)
@@ -58,8 +54,7 @@ def check():
                             check_length_bad,
                             check_dup_bad])
 
-        df_bad.to_csv(bad_tb, index=False)
-        df_native.to_csv(na_tb, index=False)
+        return df_native, df_bad
 
     except Exception as ex:
         mess = ex
